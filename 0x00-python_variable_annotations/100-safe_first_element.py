580201b7f6f324bb16f73e9e75258e0651a76afc
Marisol2201 <1529@holbertonschool.com>
2021-04-27 23:59:14 +0000

    task 100 added with the main

---
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Augment the following code"""
+
+from typing import Sequence, Union, Any
+
+
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    """Augment the following code with the correct duck-typed
+    annotations"""
+    if lst:
+        return lst[0]
+    else:
+        return None
